079f8781fec19b1770f4ef92514977c5702cc887
lbgupta <lal-babu.gupta@soprasteria.com>
2018-08-18 00:23:39 +0530

    Added one more method in service interface.

--- app/src/main/java/com/crecheinfo/recyclerviewproject/CrecheService.java
+++ app/src/main/java/com/crecheinfo/recyclerviewproject/CrecheService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface CrecheService {
 
     @GET("creches")
     Call<List<Creche>> getCreches();
 
+    @GET("creches/{id}")
+    Call<Creche> getCrecheDetails(@Path("id") int crecheId);
 }
