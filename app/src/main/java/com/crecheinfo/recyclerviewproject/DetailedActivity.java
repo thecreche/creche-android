e0f0f618ee38ade8e15083001b891217e8434458
LENOVO-PC\Atiksh <lbguptamca@gmail.com>
2018-08-18 01:24:39 +0530

    changing in detail activity

--- app/src/main/java/com/crecheinfo/recyclerviewproject/DetailedActivity.java
+++ app/src/main/java/com/crecheinfo/recyclerviewproject/DetailedActivity.java
@@ -3,6 +3,16 @@ package com.crecheinfo.recyclerviewproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DetailedActivity extends AppCompatActivity {
 
@@ -13,7 +23,50 @@ public class DetailedActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String crecheId = intent.getStringExtra(Constants.CRECHE_ID);
+        int result = Integer.parseInt(crecheId);
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://7amzmvxi9j.execute-api.ap-southeast-1.amazonaws.com/Prod/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CrecheService service = retrofit.create(CrecheService.class);
+
+        Call<Creche> creche = service.getCrecheDetails(result);
+
+        creche.enqueue(new Callback<Creche>() {
+
+            @Override
+            public void onResponse(Call<Creche> call, Response<Creche> response) {
+                Creche creche = response.body();
+
+                displayCreche(creche);
+            }
+
+            @Override
+            public void onFailure(Call<Creche> call, Throwable t) {
+
+
+            }
+
+        });
+    }
+
+    private void displayCreche(Creche creche) {
+
+        ImageView image = findViewById(R.id.image_view_detail);
+        TextView name = findViewById(R.id.text_view_detail);
+        TextView information = findViewById(R.id.text_view_detail_information);
+
+        Picasso.get().setLoggingEnabled(true);
+
+        Picasso.get().load(creche.getImageUrl())
+                .placeholder(R.drawable.default_placeholder)
+                .error(R.drawable.ic_launcher_background)
+                .into(image);
+
+        name.setText(creche.getName());
+        information.setText(creche.getDescription());
 
 
     }
