c619860ecc800b46389e4104762a50fdef21818b
LB Gupta <lbguptamca@gmail.com>
2018-08-25 00:50:33 +0530

    Code changes

--- app/src/main/java/com/crecheinfo/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/crecheinfo/recyclerviewproject/MainActivity.java
@@ -1,22 +1,16 @@
 package com.crecheinfo.recyclerviewproject;
 
-import android.app.LoaderManager;
-import android.content.ClipData;
-import android.content.CursorLoader;
-import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -25,41 +19,46 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static android.provider.UserDictionary.Words.CONTENT_URI;
-
 public class MainActivity extends AppCompatActivity {
 
-//    private final String android_version_name[] = {
-//            "Donut",
-//            "Eclair",
-//            "Froyo",
-//            "Gingerbread",
-//            "Honeycomb",
-//            "Ice Cream Sandwich",
-//            "Jelly Bean",
-//            "KitKat",
-//            "Lollipop",
-//            "Marshmallow"
-//    };
-//
-//    private final String android_image_url[] = {
-//             "http://api.learn2crack.com/android/images/donut.png",
-//            "http://api.learn2crack.com/android/images/eclair.png",
-//            "http://api.learn2crack.com/android/images/froyo.png",
-//            "http://api.learn2crack.com/android/images/ginger.png",
-//            "http://api.learn2crack.com/android/images/honey.png",
-//            "http://api.learn2crack.com/android/images/icecream.png",
-//            "http://api.learn2crack.com/android/images/jellybean.png",
-//            "http://api.learn2crack.com/android/images/kitkat.png",
-//            "http://api.learn2crack.com/android/images/lollipop.png",
-//            "http://api.learn2crack.com/android/images/marshmallow.png"
-//    };
+    SwipeRefreshLayout mySwipeRefreshLayout;
+    RecyclerView recyclerView;
+    CoordinatorLayout homeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySwipeRefreshLayout = findViewById(R.id.swiperefresh);
+        recyclerView = findViewById(R.id.card_recycler_view);
+        homeLayout = findViewById(R.id.homeLayout);
+        fetchCreches();
+
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+
+                        fetchCreches();
+                    }
+                }
+        );
+    }
+
+    private void displayCreches(List<Creche> crecheList) {
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        CrecheListAdapter adapter = new CrecheListAdapter(getApplicationContext(), crecheList);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void fetchCreches() {
         final ProgressBar progressBar = findViewById(R.id.progressbar);
+        TextView textView = findViewById(R.id.connectivity);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://7amzmvxi9j.execute-api.ap-southeast-1.amazonaws.com/Prod/")//*
@@ -75,16 +74,24 @@ public class MainActivity extends AppCompatActivity {
         InternetConnection internetConnection;
         internetConnection = new InternetConnection(getApplicationContext());
         if (internetConnection.isConnectingToInternet() == true) {
-
+            textView.setVisibility(View.GONE);
             Call<List<Creche>> creches = service.getCreches();//Calling Lists of Creches
-            progressBar.setVisibility(View.VISIBLE);
+
+            if (!mySwipeRefreshLayout.isRefreshing())
+                progressBar.setVisibility(View.VISIBLE);
 
             creches.enqueue(new Callback<List<Creche>>() {//meaning of enqueue is unknown
 
                 @Override
                 public void onResponse(Call<List<Creche>> call, Response<List<Creche>> response) {
                     List<Creche> crecheList = response.body();
-                    progressBar.setVisibility(View.GONE);
+
+
+                    if (mySwipeRefreshLayout.isRefreshing())
+                        mySwipeRefreshLayout.setRefreshing(false);
+                    else
+                        progressBar.setVisibility(View.GONE);
+
                     displayCreches(crecheList);
                 }
 
@@ -95,53 +102,12 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             });
+        } else {
+            if (mySwipeRefreshLayout.isRefreshing()) {
+                mySwipeRefreshLayout.setRefreshing(false);
+                Snackbar.make(homeLayout, "Unable to refresh data", Snackbar.LENGTH_SHORT).show();
+            } else
+                textView.setVisibility(View.VISIBLE);
         }
-        else {
-            TextView textView=findViewById(R.id.connectivity);
-
-
-        }
-
-
-        //initViews();
     }
-
-
-
-    private void displayCreches(List<Creche> crecheList) {
-
-        RecyclerView recyclerView = findViewById(R.id.card_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        CrecheListAdapter adapter = new CrecheListAdapter(getApplicationContext(), crecheList);
-        recyclerView.setAdapter(adapter);
-
-    }
-
-    private void initViews() {
-
-//        RecyclerView recyclerView = findViewById(R.id.card_recycler_view);
-//        recyclerView.setHasFixedSize(true);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-//        recyclerView.setLayoutManager(layoutManager);
-//
-//        ArrayList androidVersion = prepareData();
-//        CrecheListAdapter adapter = new CrecheListAdapter(getApplicationContext(), androidVersion);
-//        recyclerView.setAdapter(adapter);
-
-    }
-
-//    private ArrayList prepareData() {
-//        ArrayList android_version = new ArrayList();
-//        for (int i = 0; i < android_version_name.length; i++) {
-//            AndroidVersion androidVersion = new AndroidVersion();
-//            androidVersion.setAndroid_version_name(android_version_name[i]);
-//            androidVersion.setAndroid_image_url(android_image_url[i]);
-//            android_version.add(androidVersion);
-//        }
-//        return android_version;
-//    }
-
 }
